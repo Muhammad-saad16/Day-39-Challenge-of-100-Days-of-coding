2cb58e62f40967e3b1337136c9d744a3a5593c3f
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-06-15 13:45:05 +0500

    Day 39

---
+++ main.js
@@ -0,0 +1,106 @@
+"use strict";
+//            🚀 Day 39 Challenge: Start Coding! 🚀
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 115: 
+// Use a switch statement to log the days of the week based on a number (1-7).
+let day = 7;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Not a valid day");
+        break;
+}
+console.log(day); // output is sunday
+// Question 116: 
+// Create a switch case that matches several cases to the same code block, representing seasons.
+let season = "spring";
+switch (season) {
+    case "spring":
+    case "summer":
+    case "autumn":
+        console.log("It's hot");
+        break;
+    case "winter":
+        console.log("It's cold");
+        break;
+    default:
+        console.log("Not a valid season");
+        break;
+}
+console.log(season); //output is it's hot
+/*
+// Question 117:
+// Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+
+let days: number = 7;
+
+switch (days) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Not a valid day");
+        break;
+}
+console.log(days);// output is Sunday
+*/
+function evaluatesgrade(grade) {
+    switch (grade) {
+        case "A":
+            console.log("Excellent");
+            break;
+        case "B":
+            console.log("Good");
+            break;
+        case "C":
+            console.log("Fair");
+            break;
+        case "D":
+            console.log("Poor");
+            break;
+        case "F":
+            console.log("Failed");
+            break;
+        default:
+            break;
+    }
+}
+console.log(evaluatesgrade("A"));
